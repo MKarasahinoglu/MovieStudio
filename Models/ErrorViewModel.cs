a00482dffb01c06b6ec2ced5d8fb618ab49eb380
Metehan Karaşahinoğlu <mthn.karasahin@hotmail.com>
2022-06-03 15:05:13 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MovieStudio.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
